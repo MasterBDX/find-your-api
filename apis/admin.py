9d411a7a70f8733581f2331560b5ae28ddf1356f
MasterBDX <masterbdxteam@gmail.com>
2020-12-04 16:31:38 +0200

    added search box feature for user api admin

--- apis/admin.py
+++ apis/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 from .models import UserApiModel,PostApiModel,CommentApiModel
 
-admin.site.register(UserApiModel)
+
+class UsersAPIAdmin(admin.ModelAdmin):
+    search_fields = ['id','username', 'email','full_name','birth_place',
+                     'phone_number','address','birthday']
+
+admin.site.register(UserApiModel,UsersAPIAdmin)
 admin.site.register(PostApiModel)
 admin.site.register(CommentApiModel)
