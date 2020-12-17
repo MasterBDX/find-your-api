1243bbb52bfa2691479137e6e69f2e9790f4a6ac
MasterBDX <masterbdxteam@gmail.com>
2020-12-17 22:50:13 +0200

    added author fields in Post Api Model

--- apis/models.py
+++ apis/models.py
@@ -30,6 +30,9 @@ class UserApiModel(models.Model):
 
 class PostApiModel(models.Model):
     author_id = models.ForeignKey(UserApiModel,on_delete=models.CASCADE)
+    authorId = models.PositiveIntegerField(default=1)
+    author_name = models.CharField(max_length=255)
+    author_email = models.EmailField()
     title = models.CharField(max_length=255)
     overview = models.TextField()
     content = models.TextField()
